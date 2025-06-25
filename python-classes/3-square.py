fc4ee5a50695709719a895e99cc84fb066a1083d
Aksin <11200@holbertonstudents.com>
2025-06-25 09:27:23 +0000

    Salam

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,11 +4,13 @@
 
 class Square:
     '''Salam'''
-    def __init__(self, value):
+    def __init__(self, size=0):
+        self.__size = size
+    def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self.__value = value
+        self.__size = value
     def area(self):
-        return self.__value**2
+        return self.__size**2
