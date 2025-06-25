0e230e9246e7dc7f5d18fab53f77d5269c6ca3dd
Aksin <11200@holbertonstudents.com>
2025-06-25 08:23:41 +0000

    Salam

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     '''Salam'''
-    def __init__(self, size=0):
+    def __init__(self, size=0, area):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
